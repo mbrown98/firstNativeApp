5669ca10aa677e7941421c01216633609a67b121
Matt Brown <matthewbrown0798@gmail.com>
2020-11-04 18:04:28 -0500

    added square, color screens

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import ComponentsScreen from "./src/screens/ComponentScreen";
 import ListScreen from "./src/screens/ListScreen";
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from "./src/screens/CounterScreen";
+import ColorScreen from "./src/screens/ColorScreen";
+import SquareScreen from "./src/screens/SqaureScreen";
 
 const navigator = createStackNavigator(
   {
@@ -13,6 +15,8 @@ const navigator = createStackNavigator(
     List: ListScreen,
     Image: ImageScreen,
     Counter: CounterScreen,
+    Color: ColorScreen,
+    Square: SquareScreen,
   },
   {
     initialRouteName: "Home",
